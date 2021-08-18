d6704da7fae2915977875b682a6bbd7f19f5eb60
David Bento <dfbento26@gmail.com>
2021-08-18 00:34:10 -0300

    fix: criação de input de dados via form.

--- scripts/script.js
+++ scripts/script.js
@@ -7,57 +7,75 @@ const Modal = {
   },
 }
 
-const transactions = [
-  {
-    id: 1,
-    description: 'Luz',
-    amount: -59935,
-    date: '23/01/2021',
-  },
-  {
-    id: 2,
-    description: 'Salário',
-    amount: 550067,
-    date: '30/01/2021',
-  },
-  {
-    id: 3,
-    description: 'Internet',
-    amount: -200075,
-    date: '15/01/2021',
-  },
-  {
-    id: 4,
-    description: 'Cursos',
-    amount: -10000,
-    date: '25/01/2021',
-  },
-  {
-    id: 5,
-    description: 'Investimentos',
-    amount: 10000,
-    date: '10/01/2021',
-  },
-]
-
 const Transaction = {
+  all: [
+    {
+      description: 'Luz',
+      amount: -59935,
+      date: '23/01/2021',
+    },
+    {
+      description: 'Salário',
+      amount: 550067,
+      date: '30/01/2021',
+    },
+    {
+      description: 'Internet',
+      amount: -200075,
+      date: '15/01/2021',
+    },
+    {
+      description: 'Cursos',
+      amount: -10000,
+      date: '25/01/2021',
+    },
+    {
+      description: 'Investimentos',
+      amount: 10000,
+      date: '10/01/2021',
+    },
+  ],
+  add(transaction) {
+    Transaction.all.push(transaction)
+    App.reload()
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1)
+    App.reload()
+  },
+
   incomes() {
-    // somar as entradas
+    let income = 0
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount
+      }
+    })
+    return income
   },
+
   expenses() {
-    // somar as saídas
+    let expense = 0
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount
+      }
+    })
+    return expense
   },
+
   total() {
-    // incomes - expenses
+    return Transaction.incomes() + Transaction.expenses()
   },
 }
 
-const Grid = {
+const DOM = {
   transactionsContainer: document.querySelector('#data-table tbody'),
   addTransaction(transaction, index) {
     const tr = document.createElement('tr')
-    tr.innerHTML = Grid.innerHTMLTransaction(transaction)
-    Grid.transactionsContainer.appendChild(tr)
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    DOM.transactionsContainer.appendChild(tr)
   },
   innerHTMLTransaction(transaction) {
     const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
@@ -75,6 +93,21 @@ const Grid = {
 
     return table
   },
+
+  updateBalance() {
+    document.getElementById('incomeDisplay').innerHTML = Utils.formartCurrency(
+      Transaction.incomes()
+    )
+    document.getElementById('expenseDisplay').innerHTML = Utils.formartCurrency(
+      Transaction.expenses()
+    )
+    document.getElementById('totalDisplay').innerHTML = Utils.formartCurrency(
+      Transaction.total()
+    )
+  },
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ''
+  },
 }
 
 const Utils = {
@@ -91,6 +124,59 @@ const Utils = {
   },
 }
 
-transactions.forEach(function (transaction) {
-  Grid.addTransaction(transaction)
+const App = {
+  init() {
+    Transaction.all.forEach((transaction) => {
+      DOM.addTransaction(transaction)
+    })
+    DOM.updateBalance()
+  },
+  reload() {
+    DOM.clearTransactions()
+    App.init()
+  },
+}
+
+const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amout'),
+  date: document.querySelector('input#date'),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    }
+  },
+
+  validateFileds() {
+    const { description, amount, date } = Form.getValues()
+    console.log(description)
+  },
+
+  submit(event) {
+    event.preventDefault()
+    // verificar se todos os campos foram preenchidos
+    Form.validateFileds()
+    // formatar os dados para salvar
+
+    // salvar
+
+    // limpar formulário
+
+    // fechar modal
+
+    // atualizar a aplicação
+  },
+}
+
+App.init()
+
+Transaction.add({
+  description: 'Extra',
+  amount: 30000,
+  date: '17/08/2021',
 })
+
+//Transaction.remove(0)
